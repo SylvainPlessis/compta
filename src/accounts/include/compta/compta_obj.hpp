6c5e535c80e71a5bc4f40d9b4f880586f452c1dd
Sylvain Plessis <sylvain.plessis@gmail.com>
2015-01-14 20:42:20 -0600

    Starting remanufacturing reports:   - TeX's not done   - from and to Date to be added in print to screen

--- src/accounts/include/compta/compta_obj.hpp
+++ src/accounts/include/compta/compta_obj.hpp
@@ -51,7 +51,7 @@ namespace Compta{
       ~ComptaObj();
 
       //!print fork
-      void report(PrintOptions opt, std::ostream &out = std::cout) const;
+      void report(const Date & from, const Date & to, bool forecast, bool bank, bool cash) const;
 
       //!print all
       friend std::ostream &operator<< (std::ostream &out, const ComptaObj &obj)
@@ -87,7 +87,7 @@ namespace Compta{
       void add_posting(Posting &post);
 
       //!build the report objects
-      void report_compta(std::vector<MonthlyReport> &rep) const;
+      void report_compta(std::vector<MonthlyReport> &rep, const Date &from, const Date &to) const;
 
       //!report a month, date is in the MonthlyReport object
       void report_compta(MonthlyReport &rep) const;
@@ -140,22 +140,22 @@ namespace Compta{
       void posting_account_to_account(AccountTypeSrc &src, AccountTypeTrg &trg, Posting &post);
 
       //!print all
-      void report_all(std::ostream &out = std::cout) const;
+      void report_all(const Date & from, const Date & to, std::ostream &out = std::cout) const;
 
       //!print forecast
       void report_forecast(std::ostream &out = std::cout) const;
 
       //!print bank accounts
-      void report_bank(std::ostream &out = std::cout) const;
+      void report_bank(const Date & from, const Date & to, std::ostream &out = std::cout) const;
 
       //!print cash
-      void report_cash(std::ostream &out = std::cout) const;
+      void report_cash(const Date & from, const Date & to, std::ostream &out = std::cout) const;
 
       //!print one bank
-      void report_a_bank(std::ostream &out,unsigned int ib) const;
+      void report_a_bank(const Date & from, const Date & to, std::ostream &out,unsigned int ib) const;
 
       //!print one cash
-      void report_a_cash(std::ostream &out,unsigned int ic) const;
+      void report_a_cash(const Date & from, const Date & to, std::ostream &out,unsigned int ic) const;
 
       Forecast _previsionnel;
       std::vector<Cash> _liquide;
